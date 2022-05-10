3b423ac8af5f715d277f43a350151f4a2671c829
designbyfaizi <faizanullah1999@gmail.com>
2022-05-11 01:56:55 +0500

    Switched Assets to Public

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <main class="flex flex-col justify-center items-center min-h-screen gap-8 bg-rose-900">
     <img
-      src='/assets/Home.svg'
+      src='/Home.svg'
       alt="Home Page"
       class="max-w-[900px] shadow-dark-900 shadow-2xl rounded-4xl hidden lg:block"
     />
     <img
-      src='/assets/Home-Mobile.svg'
+      src='/Home-Mobile.svg'
       alt="Home Page"
       class="max-w-[375px] w-[90%] shadow-dark-900 shadow-2xl rounded-xl lg:hidden"
     />
