17c7d3b7ae329292f26d4adc557316cd1a582fa9
designbyfaizi <faizanullah1999@gmail.com>
2022-05-28 13:53:21 +0500

    Testing Dynamic Routes

---
+++ pages/test/[id].vue
@@ -0,0 +1,12 @@
+<template>
+  <Head>
+    <Title>{{$route.params.id}}</Title>
+  </Head>
+  <NuxtLayout name="main">
+    <main
+      class="container mx-auto max-w-screen-xl flex flex-col justify-center items-center min-h-screen h-full gap-8"
+    >
+      {{$route.params.id}}
+    </main>
+  </NuxtLayout>
+</template>
